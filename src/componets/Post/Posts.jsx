2ff2c7194186bb26bdbc65ff377346d82ced8984
Yizhak Shenk <1997shenk@gmail.com>
2023-05-29 17:50:47 +0300

    specific post

--- src/componets/Post/Posts.jsx
+++ src/componets/Post/Posts.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import PostCard from "./PostCard";
 import Filters from "../filters/Filters";
 import PostsContext from "../../context/PostsContext";
-import {
-  Box,
-  Grid,
-  CircularProgress,
-  Typography,
-} from "@mui/material";
-const urlServer = process.env.REACT_APP_URL_SERVER
+import { Box, Grid, CircularProgress, Typography, Button } from "@mui/material";
+const urlServer = process.env.REACT_APP_URL_SERVER;
 
 export default function Posts() {
+  const [paramseS] = useSearchParams();
+  const postid = paramseS.get("pid");
+  const [postId, setPostId] = useState(postid);
   const [rendering, setRendering] = useState(false);
-  const { posts, setPosts } = useContext(PostsContext)
+  const { posts, setPosts } = useContext(PostsContext);
+  const navigae = useNavigate();
 
+  const handleHomePage = () => {
+    navigae("/");
+  };
   const handleRendering = () => {
     setRendering(!rendering);
   };
@@ -22,20 +25,29 @@ export default function Posts() {
   useEffect(() => {
     (async () => {
       try {
-        const postsList = await (
-          await axios.get(`${urlServer}/post/get-all`)
-        ).data;
-
-        if (!postsList) {
-          throw new Error("posts not dound");
-        } else {
+        let postsList;
+        if (postId) {
+          postsList = [
+            await (
+              await axios.post(`${urlServer}/post/get-one`, { id: postId })
+            ).data,
+          ];
           setPosts(postsList);
+          // console.log('postsList: ', postsList);
+        } else {
+          postsList = await (await axios.get(`${urlServer}/post/get-all`)).data;
+
+          if (!postsList) {
+            throw new Error("posts not dound");
+          } else {
+            setPosts(postsList);
+          }
         }
       } catch (err) {
         console.log(err);
       }
     })();
-  }, [rendering, setPosts]);
+  }, [rendering, setPosts, postId]);
 
   return (
     <Box alignItems="center">
@@ -43,12 +55,12 @@ export default function Posts() {
         <Box>
           <Filters setPosts={setPosts} handleRendering={handleRendering} />
           <Grid container sx={{ placeContent: "center" }} spacing={1}>
-            { posts?.length ? (
+            {posts?.length ? (
               posts.map((post, index) => {
                 return (
-                    <Grid item key={index}>
-                      <PostCard post={post} />
-                    </Grid>
+                  <Grid item key={index}>
+                    <PostCard post={post} />
+                  </Grid>
                 );
               })
             ) : (
@@ -57,6 +69,17 @@ export default function Posts() {
               </Box>
             )}
           </Grid>
+          {postId && (
+            <Button sx={{marginBottom: 5}}
+              variant="outlined"
+              onClick={() => {
+                setPostId(null);
+                handleHomePage();
+              }}
+            >
+              view all posts
+            </Button>
+          )}
         </Box>
       ) : (
         <Box sx={{ marginTop: "20%" }}>
